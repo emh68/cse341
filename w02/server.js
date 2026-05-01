6f1628be5cbb29fd1b4093f3a153ea381304a12a
Eli Hansen <elihansen@yahoo.com>
2026-04-30 19:21:06 -0600

    setup test

--- w02/server.js
+++ w02/server.js
@@ -2,17 +2,18 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const contactsRoute = require('./routes/contacts.js');
+// const mongodb = require('./db/connect');
 const cors = require('cors');
 
 app.use(cors());
 app.use("/contacts", contactsRoute)
 
-mongodb.initDb((err, mongodb) => {
-    if (err) {
-        console.log(err);
-    } else {
+// mongodb.initDb((err, db) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
       app.listen(port, () => {
         console.log(`app listening on http://localhost:${port}`);
       });
-    }
-});
+//     }
+// });
