5d4f999a47722a0a79a1bb9769e7183d66b2dcba
Eli Hansen <elihansen@yahoo.com>
2026-05-08 16:27:35 -0600

    configured dynamic port for deployment and aligned /contacts route paths missing forward slash.

--- w02/routes/contacts.js
+++ w02/routes/contacts.js
@@ -3,9 +3,9 @@ const router = new express.Router();
 const { getAllContacts, getSingleContact, createContact, deleteContact, updateContact } = require('../controllers/contactsController');
 
 router.get('/contacts', getAllContacts);
-router.get('/contacts:id', getSingleContact);
+router.get('/contacts/:id', getSingleContact);
 router.post('/contacts', createContact);
-router.delete('/contacts:id', deleteContact);
-router.put('/contacts:id', updateContact);
+router.delete('/contacts/:id', deleteContact);
+router.put('/contacts/:id', updateContact);
 
 module.exports = router;
